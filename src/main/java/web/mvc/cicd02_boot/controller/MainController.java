16d421ec640cc8bfee1e6252281a13ffd02b8971
BongBong <8253jang@daum.net>
2025-12-09 10:44:34 +0900

    docker container test 01

--- src/main/java/web/mvc/cicd02_boot/controller/MainController.java
+++ src/main/java/web/mvc/cicd02_boot/controller/MainController.java
@@ -8,6 +8,6 @@ public class MainController {
     @GetMapping("/")
     public String index(){
         return
-                "GitHub Actions를 이용한 second CI/CD TEST -  12월 9일 오전 test ";
+                "GitHub Actions를 이용한 Docker Container CI/CD TEST  ";
     }
 }
